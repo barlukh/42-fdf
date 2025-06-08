a0eb7a22c93c0a09ce24faaa4154cdaeca5d28f2
Boris Gazur <barlukh@gmail.com>
2025-06-08 13:53:41 +0300

    Declare static buffer on stack

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -6,38 +6,30 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 09:17:16 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/08 11:04:38 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/08 13:52:59 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fdf.h"
 
-static char	*ft_line_read(int fd, char **buf, t_gnl *var);
+static char	*ft_line_read(int fd, char *buf, t_gnl *var);
 static char	*ft_strjoin_gnl(char *buf, t_gnl *var);
-static char	*ft_substr_gnl(char **s, size_t len);
+static char	*ft_substr_gnl(char *s, size_t len);
 
 char	*ft_get_next_line(int fd)
 {
-	static char	*buf;
+	static char	buf[BUFFER_SIZE];
 	t_gnl		var;
 
-	if (!buf)
-	{
-		buf = malloc(BUFFER_SIZE);
-		if (!buf)
-			return (NULL);
-		buf[0] = '\0';
-	}
-	var.cache = ft_substr_gnl(&buf, ft_strchr(buf, '\0') - buf);
+	var.cache = ft_substr_gnl(buf, ft_strchr(buf, '\0') - buf);
 	if (!var.cache)
 		return (NULL);
-	if (ft_line_read(fd, &buf, &var) == NULL)
+	if (ft_line_read(fd, buf, &var) == NULL)
 		return (NULL);
-	var.new_line = ft_substr_gnl(&var.cache, var.br - var.cache + 1);
+	var.new_line = ft_substr_gnl(var.cache, var.br - var.cache + 1);
 	if (!var.new_line)
 	{
-		free(buf);
-		buf = NULL;
+		free(var.cache);
 		return (NULL);
 	}
 	ft_memcpy(buf, var.br + 1, ft_strchr(var.br + 1, '\0') - var.br);
@@ -45,12 +37,13 @@ char	*ft_get_next_line(int fd)
 	return (var.new_line);
 }
 
-static char	*ft_line_read(int fd, char **buf, t_gnl *var)
+// Reads from a file looking for a linebreak or EOF
+static char	*ft_line_read(int fd, char *buf, t_gnl *var)
 {
 	var->br = ft_strchr(var->cache, '\n');
 	while (!var->br)
 	{
-		var->read_bytes = read(fd, *buf, BUFFER_SIZE);
+		var->read_bytes = read(fd, buf, BUFFER_SIZE);
 		if (var->read_bytes <= 0)
 		{
 			if (var->read_bytes == 0 && *var->cache)
@@ -58,35 +51,32 @@ static char	*ft_line_read(int fd, char **buf, t_gnl *var)
 				var->br = ft_strchr(var->cache, '\0') - 1;
 				return (var->br);
 			}
-			free(*buf);
 			free(var->cache);
-			return (*buf = NULL);
+			return (NULL);
 		}
-		var->cache = ft_strjoin_gnl(*buf, var);
+		var->cache = ft_strjoin_gnl(buf, var);
 		if (!var->cache)
-		{
-			free(*buf);
-			free(var->cache);
-			return (*buf = NULL);
-		}
+			return (NULL);
 		var->br = ft_strchr(var->cache, '\n');
 	}
 	return (var->br);
 }
 
+// Joins two strings together (cache with the readout from a buffer)
 static char	*ft_strjoin_gnl(char *buf, t_gnl *var)
 {
 	size_t	cache_len;
 	char	*new_cache;
 
-	if (!buf || !var->cache)
-		return (NULL);
 	cache_len = 0;
 	while (var->cache[cache_len] != '\0')
 		cache_len++;
 	new_cache = malloc(sizeof(char) * (cache_len + var->read_bytes + 1));
 	if (!new_cache)
+	{
+		free(var->cache);
 		return (NULL);
+	}
 	ft_memcpy(new_cache, var->cache, cache_len);
 	ft_memcpy(new_cache + cache_len, buf, var->read_bytes);
 	new_cache[cache_len + var->read_bytes] = '\0';
@@ -94,24 +84,19 @@ static char	*ft_strjoin_gnl(char *buf, t_gnl *var)
 	return (new_cache);
 }
 
-static char	*ft_substr_gnl(char **s, size_t len)
+// Creates a new substring of specified length from a string
+static char	*ft_substr_gnl(char *s, size_t len)
 {
 	char	*substr;
 	size_t	i;
 
-	if (!*s)
-		return (NULL);
 	substr = malloc(sizeof(char) * (len + 1));
 	if (!substr)
-	{
-		free(*s);
-		*s = NULL;
 		return (NULL);
-	}
 	i = 0;
 	while (i < len)
 	{
-		substr[i] = (*s)[i];
+		substr[i] = s[i];
 		i++;
 	}
 	substr[i] = '\0';
