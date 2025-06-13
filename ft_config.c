85d0741a6e54f4b318a975395719e5005ef7a375
Boris Gazur <barlukh@gmail.com>
2025-06-13 10:16:43 +0300

    Minor refactor

--- ft_config.c
+++ ft_config.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:15:21 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/13 07:14:31 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/13 10:10:16 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,6 @@
 
 static void	ft_boundig_box_draw(t_config *cfg);
 static void	ft_bounding_min_max(t_config *cfg);
-
-void	ft_config_matrix(t_config *cfg)
-{
-	cfg->x_max = 0;
-	cfg->x_min = 0;
-	cfg->y_max = 0;
-	cfg->y_min = 0;
-	cfg->center_x = WIDTH / 2;
-	cfg->center_y = HEIGHT / 2;
-	cfg->i = 0;
-	while (cfg->i < (cfg->line_size * cfg->lst_size))
-	{
-		cfg->x_temp = cfg->p[cfg->i].x;
-		cfg->y_temp = cfg->p[cfg->i].y;
-		cfg->p[cfg->i].x = (cfg->x_temp + cfg->y_temp) * cos(120);
-		cfg->p[cfg->i].y = (cfg->y_temp - cfg->x_temp) * sin(120) - cfg->p[cfg->i].z;
-		ft_bounding_min_max(cfg);
-		cfg->i++;
-	}
-	cfg->x_temp = fabs(cfg->x_max - cfg->x_min);
-	cfg->y_temp = fabs(cfg->y_max - cfg->y_min);
-	ft_boundig_box_draw(cfg);
-}
 
 void	ft_config_window(t_config *cfg)
 {
@@ -47,6 +24,38 @@ void	ft_config_window(t_config *cfg)
 		cfg->m_width - (cfg->m_width / 10),
 		cfg->m_height - (cfg->m_height / 10));
 	ft_fill_screen(cfg);
+	cfg->center_x = WIDTH / 2;
+	cfg->center_y = HEIGHT / 2;
+}
+
+void	ft_config_matrix(t_config *cfg)
+{
+	cfg->x_min = 0;
+	cfg->x_max = 0;
+	cfg->y_min = 0;
+	cfg->y_max = 0;
+	cfg->i = 0;
+	while (cfg->i < (cfg->line_size * cfg->lst_size))
+	{
+		ft_projection_isometric(cfg);
+		ft_bounding_min_max(cfg);
+		cfg->i++;
+	}
+	cfg->x_temp = fabs(cfg->x_max - cfg->x_min);
+	cfg->y_temp = fabs(cfg->y_max - cfg->y_min);
+	ft_boundig_box_draw(cfg);
+}
+
+static void	ft_bounding_min_max(t_config *cfg)
+{
+	if (cfg->p[cfg->i].x > cfg->x_max)
+		cfg->x_max = cfg->p[cfg->i].x;
+	if (cfg->p[cfg->i].x < cfg->x_min)
+		cfg->x_min = cfg->p[cfg->i].x;
+	if (cfg->p[cfg->i].y > cfg->y_max)
+		cfg->y_max = cfg->p[cfg->i].y;
+	if (cfg->p[cfg->i].y < cfg->y_min)
+		cfg->y_min = cfg->p[cfg->i].y;
 }
 
 static void	ft_boundig_box_draw(t_config *cfg)
@@ -75,16 +84,4 @@ static void	ft_boundig_box_draw(t_config *cfg)
 			+ (cfg->y_temp / 2)) * cfg->space;
 		cfg->i++;
 	}
-}
-
-static void	ft_bounding_min_max(t_config *cfg)
-{
-	if (cfg->p[cfg->i].x > cfg->x_max)
-		cfg->x_max = cfg->p[cfg->i].x;
-	if (cfg->p[cfg->i].x < cfg->x_min)
-		cfg->x_min = cfg->p[cfg->i].x;
-	if (cfg->p[cfg->i].y > cfg->y_max)
-		cfg->y_max = cfg->p[cfg->i].y;
-	if (cfg->p[cfg->i].y < cfg->y_min)
-		cfg->y_min = cfg->p[cfg->i].y;
 }
