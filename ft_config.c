235d7e1209dc0b902204ddfc10f2340dfde11205
Boris Gazur <barlukh@gmail.com>
2025-06-13 17:19:32 +0300

    Create bounding box for x

--- ft_config.c
+++ ft_config.c
@@ -6,22 +6,21 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:15:21 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/13 16:25:09 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/13 17:16:44 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fdf.h"
 
-static void	ft_bounding_box(t_config *cfg);
+static void	ft_bounding_box_x(t_config *cfg);
+static void	ft_bounding_box_y(t_config *cfg);
 
 void	ft_config_window(t_config *cfg)
 {
 	mlx_get_monitor_size(0, &cfg->m_width, &cfg->m_height);
 	mlx_set_window_pos(cfg->mlx, (cfg->m_width - WIDTH) / 2,
-		(cfg->m_height - HEIGHT) / 2);
-	mlx_set_window_limit(cfg->mlx, cfg->m_width / 4, cfg->m_height / 4,
-		cfg->m_width - (cfg->m_width / 10),
-		cfg->m_height - (cfg->m_height / 10));
+		(cfg->m_height - HEIGHT) / 1.2);
+	mlx_set_window_limit(cfg->mlx, HEIGHT / 4, WIDTH / 4, HEIGHT, WIDTH);
 	cfg->center_x = WIDTH / 2;
 	cfg->center_y = HEIGHT / 2;
 }
@@ -36,30 +35,38 @@ void	ft_config_matrix(t_config *cfg, void (*f)(t_config *))
 	f(cfg);
 	cfg->x_temp = fabs(cfg->x_max - cfg->x_min);
 	cfg->y_temp = fabs(cfg->y_max - cfg->y_min);
-	ft_bounding_box(cfg);
+	cfg->i = 0;
+	if (cfg->x_temp > cfg->y_temp)
+		ft_bounding_box_x(cfg);
+	else
+		ft_bounding_box_y(cfg);
 }
 
-static void	ft_bounding_box(t_config *cfg)
+static void	ft_bounding_box_x(t_config *cfg)
 {
-	if (cfg->x_temp > cfg->y_temp)
+	cfg->space = (WIDTH - WIDTH / 10) / cfg->x_temp;
+	while (cfg->i < (cfg->line_size * cfg->lst_size))
 	{
-		cfg->xy_bounder = cfg->x_temp;
-		cfg->space = (WIDTH - WIDTH / 10) / cfg->xy_bounder;
+		cfg->pr[cfg->i].y = cfg->center_y + (cfg->pr[cfg->i].y * cfg->space);
+		if (fabs(cfg->x_min) > fabs(cfg->x_max))
+			cfg->pr[cfg->i].x = cfg->center_x + ((cfg->pr[cfg->i].x
+						- cfg->x_min) - (cfg->x_temp / 2)) * cfg->space;
+		else
+			cfg->pr[cfg->i].x = cfg->center_x + ((cfg->pr[cfg->i].x
+						- cfg->x_max) + (cfg->x_temp / 2)) * cfg->space;
+		cfg->i++;
 	}
-	else
-	{
-		cfg->xy_bounder = cfg->y_temp;
-		cfg->space = (HEIGHT - HEIGHT / 10) / cfg->xy_bounder;
-	}
-	cfg->i = 0;
+}
+
+static void	ft_bounding_box_y(t_config *cfg)
+{
+	cfg->space = (HEIGHT - HEIGHT / 10) / cfg->y_temp;
 	while (cfg->i < (cfg->line_size * cfg->lst_size))
 	{
 		cfg->pr[cfg->i].x = cfg->center_x + (cfg->pr[cfg->i].x * cfg->space);
 		if (fabs(cfg->y_min) > fabs(cfg->y_max))
-		{
 			cfg->pr[cfg->i].y = cfg->center_y + ((cfg->pr[cfg->i].y
 						- cfg->y_min) - (cfg->y_temp / 2)) * cfg->space;
-		}
 		else
 			cfg->pr[cfg->i].y = cfg->center_y + ((cfg->pr[cfg->i].y
 						- cfg->y_max) + (cfg->y_temp / 2)) * cfg->space;
