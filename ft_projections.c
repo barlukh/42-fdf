ea48c09d0eb8f23f36e6812afc8a7725c1899787
Boris Gazur <barlukh@gmail.com>
2025-06-14 15:47:08 +0300

    Move bounding function to config file

--- ft_projections.c
+++ ft_projections.c
@@ -6,13 +6,11 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 10:08:02 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/13 17:30:36 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/14 15:44:53 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fdf.h"
-
-static void	ft_bounding_min_max(t_config *cfg);
 
 void	ft_projection_isometric(t_config *cfg)
 {
@@ -25,19 +23,6 @@ void	ft_projection_isometric(t_config *cfg)
 		cfg->pr[cfg->i].x = (cfg->x_temp - cfg->y_temp) / sqrt(2);
 		cfg->pr[cfg->i].y = (cfg->x_temp + cfg->y_temp - 2
 				* cfg->pr[cfg->i].z) / sqrt(6);
-		ft_bounding_min_max(cfg);
 		cfg->i++;
 	}
-}
-
-static void	ft_bounding_min_max(t_config *cfg)
-{
-	if (cfg->pr[cfg->i].x > cfg->x_max)
-		cfg->x_max = cfg->pr[cfg->i].x;
-	if (cfg->pr[cfg->i].x < cfg->x_min)
-		cfg->x_min = cfg->pr[cfg->i].x;
-	if (cfg->pr[cfg->i].y > cfg->y_max)
-		cfg->y_max = cfg->pr[cfg->i].y;
-	if (cfg->pr[cfg->i].y < cfg->y_min)
-		cfg->y_min = cfg->pr[cfg->i].y;
 }
--- ft_projections_bonus.c
+++ ft_projections_bonus.c
@@ -6,13 +6,11 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 17:29:07 by bgazur            #+#    #+#             */
-/*   Updated: 2025/06/13 21:25:05 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/06/14 15:45:24 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fdf_bonus.h"
-
-static void	ft_bounding_min_max(t_config *cfg);
 
 void	ft_projection_isometric(t_config *cfg)
 {
@@ -25,7 +23,6 @@ void	ft_projection_isometric(t_config *cfg)
 		cfg->pr[cfg->i].x = (cfg->x_temp - cfg->y_temp) / sqrt(2);
 		cfg->pr[cfg->i].y = (cfg->x_temp + cfg->y_temp - 2
 				* cfg->pr[cfg->i].z) / sqrt(6);
-		ft_bounding_min_max(cfg);
 		cfg->i++;
 	}
 }
@@ -41,19 +38,6 @@ void	ft_projection_trimetric(t_config *cfg)
 		cfg->pr[cfg->i].x = (cfg->x_temp - cfg->y_temp) / sqrt(3);
 		cfg->pr[cfg->i].y = (cfg->x_temp * 0.8 + cfg->y_temp * 1.2
 				- cfg->pr[cfg->i].z * 1.5) / sqrt(7);
-		ft_bounding_min_max(cfg);
 		cfg->i++;
 	}
-}
-
-static void	ft_bounding_min_max(t_config *cfg)
-{
-	if (cfg->pr[cfg->i].x > cfg->x_max)
-		cfg->x_max = cfg->pr[cfg->i].x;
-	if (cfg->pr[cfg->i].x < cfg->x_min)
-		cfg->x_min = cfg->pr[cfg->i].x;
-	if (cfg->pr[cfg->i].y > cfg->y_max)
-		cfg->y_max = cfg->pr[cfg->i].y;
-	if (cfg->pr[cfg->i].y < cfg->y_min)
-		cfg->y_min = cfg->pr[cfg->i].y;
 }
